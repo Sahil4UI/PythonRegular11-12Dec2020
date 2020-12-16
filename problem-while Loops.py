96555dedb2c0fbe73aff3e0b0e6f28e4e21cf703
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-12-16 12:01:10 +0530

    Add files via upload

---
+++ problem-while Loops.py
@@ -0,0 +1,31 @@
+
+'''
+f = 0
+s = 1
+print(f)
+print(s)
+
+i=1
+while i<=8:
+    t = f+s
+    print(t)
+    f,s = s,t
+    i+=1
+'''
+'''
+i =1
+while i<=100:
+    if i>=30 and i<=60:
+        i+=1
+        continue
+    print(i)
+    i+=1
+    
+'''
+i =1
+j =10
+while i<=10 or j>=1:
+    print(i,j)
+    i+=1
+    j-=1
+    

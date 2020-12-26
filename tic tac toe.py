6b329c3b6005083b8db44b3fe30b5bcad95c118b
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-12-26 19:37:13 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,14 +1,11 @@
 import random
-
 combinations = [
     [1,2,3],[4,5,6],[7,8,9],
     [1,4,7],[2,5,8],[3,6,9],
     [1,5,9],[3,5,7]
 
     ]
-
 positions = [i for i in range(1,10)]
-
 def gameBoard():
     print(
         f"""
@@ -20,4 +17,19 @@ def gameBoard():
                 
         """
         )
+
+def userMove(choice):
+    pos = int(input("Enter Your Position"))
+    positions[pos-1]=choice
+    gameBoard()
+
 gameBoard()
+ch = input("Enter your Choice : X | 0 ->")
+if ch=='X':
+    cpu_ch=0
+else:
+    cpu_ch='X'
+Game=True
+while Game:
+    print(positions)
+    msg=userMove(ch)

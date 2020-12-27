618b2ef2172c9b2a065326dc379e3e8be97defcd
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-12-27 12:33:35 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,10 +1,9 @@
-#step 0
 import random
+import time
 combinations = [
     [1,2,3],[4,5,6],[7,8,9],
     [1,4,7],[2,5,8],[3,6,9],
     [1,5,9],[3,5,7]
-
     ]
 positions = [i for i in range(1,10)]
 positions_occupied =[]
@@ -28,6 +27,18 @@ def userMove(choice):
     msg = checkWinner(pos,choice)
     return msg
 
+def cpu_move(cpu_ch):
+    cpu_pos = random.randint(1,9)
+    print(f"cpu positions : {cpu_pos}")
+    if cpu_pos in positions_occupied:
+        cpu_move(cpu_ch)#recursive function
+    else:
+        positions[cpu_pos-1]=cpu_ch
+        gameBoard()
+        positions_occupied.append(cpu_pos)
+        msg = checkWinner(cpu_pos,cpu_ch)
+        return msg
+
 def checkWinner(pos,choice):
     for i in range(len(combinations)):
         if pos in combinations[i]:
@@ -38,8 +49,7 @@ def checkWinner(pos,choice):
         if combinations[i][0] == choice and combinations[i][1] == choice and combinations[i][2] == choice:
            return "winner" 
             
-    
-#STEP 1
+
 gameBoard()
 ch = input("Enter your Choice : X | 0 ->")
 if ch=='X':
@@ -48,9 +58,19 @@ else:
     cpu_ch='X'
 Game=True
 while Game:
-    print(positions)
+    '''
+    print("combinations:",combinations)
+    print("occupied :",positions_occupied)
+    print("positions :",positions)
+    '''
     msg=userMove(ch)
     if msg=="winner":
         print("User Wins")
         break
-    
+    print("CPU TURN.....")
+    time.sleep(5)
+    msg = cpu_move(cpu_ch)
+    print(msg)
+    if msg=="winner":
+        print("CPU Wins")
+        break

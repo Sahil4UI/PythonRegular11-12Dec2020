78cd07daafeb20f07ec5acbca7cd565bc145c57f
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-12-30 11:56:07 +0530

    Add files via upload

---
+++ rec_exercise.py
@@ -0,0 +1,19 @@
+'''
+def Power(a,b):
+    if b==1:
+        return a
+    return a*Power(a,b-1)
+a = int(input("Enter Base:"))
+b = int(input("Enter Power:"))
+print(Power(a,b
+'''
+
+def f1(n):
+    if n>10:
+        return
+
+    f1(n+1)
+    print(n)
+
+f1(1)
+            

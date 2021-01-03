c9f374065b69a7a0fedd0c1d56225154d9091266
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2021-01-03 20:08:07 +0530

    Add files via upload

--- Map & filter & lambda.py
+++ Map & filter & lambda.py
@@ -9,11 +9,92 @@ result = []
 for i in list1:
     result.append(Conversion(i))
 print(result)
-'''
 result = list(map(Conversion,list1))
 print(result)
 
+'''
 #lambda Funtion - single line function
+'''
+def Conversion(cm):
+    return 0.01*cm
+
 x = lambda cm : 0.01*cm
 print(x(345))
+'''
+'''
+def Even(x):
+    if x%2==0:
+        return True
+    else:
+        return False
+
+x=[1,2,3,4,5,6]
+result = list(filter(Even,x))
+print(result)
+'''
+'''
+result = list(map(Even,x))
+for i in range(len(result)):
+    if result[i]==True:
+        print(x[i])
+'''
+'''
+import functools
+list1=[1,2,3,4,5]
+x =functools.reduce(lambda a,b:a+b,list1)
+print(x)
+'''
+
+d = {k:v for k,v in ('a1','b2')}
+d['a1']=1
+d['b2']=2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

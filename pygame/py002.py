8d678ba9168b9d9dae7c25dade39c5ce0f2e7b8b
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2021-01-12 11:29:32 +0530

    Add files via upload

--- pygame/py002.py
+++ pygame/py002.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 pygame.init()
 H= 600
@@ -7,47 +8,54 @@ color= (255,255,255)
 red = (255 , 0 , 0 )
 x=0
 y=0
-w=50
-h=50
+w=30
+h=30
 movex = 0
 movey = 0
+
+frog=pygame.image.load(r'pygame\frog.png')#raw string-path
+frog = pygame.transform.scale(frog,(50,50))
+frogX = random.randint(0,W-50)
+frogY = random.randint(0,H-50)
+
 while True:
     gameScreen.fill(color)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             quit()
-            # 
         if event.type==pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 movex=-1
                 movey=0
-                
             elif event.key == pygame.K_RIGHT:
                 movex=1
                 movey=0
-            
             elif event.key==pygame.K_UP:
                 movey=-1
                 movex=0
-            
             elif event.key==pygame.K_DOWN:
                 movey=1
                 movex=0
-        # pygame.draw.rect(gameScreen,red,[x,y,w,h])
-    
+    # gameScreen.blit(image,(imageX,imageY))
+    frogRect = pygame.Rect([frogX,frogY,50,50])
+    gameScreen.blit(frog,(frogX,frogY))
     x += movex
     y += movey
     
-    if x>W:
-        x=25
+    if x>W-w:
+        movex=-1
     elif x<0:
-        x=W
-    if y>H:
-        y = 25
+        movex=1
+    if y>H-h:
+        movey=-1
     elif y<0:
-        y = H
+        movey=1
     
-    pygame.draw.rect(gameScreen,red,[x,y,w,h])
+    snake = pygame.draw.rect(gameScreen,red,[x,y,w,h])
+    if snake.colliderect(frogRect):
+        frogX = random.randint(0,W-50)
+        frogY = random.randint(0,H-50)
+
+        
     pygame.display.update()
-    

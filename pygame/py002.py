22bf2f13babf919cab4eaeb46c3cc84756f9a000
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2021-01-15 11:30:08 +0530

    Add files via upload

--- pygame/py002.py
+++ pygame/py002.py
@@ -6,56 +6,94 @@ W=800
 gameScreen= pygame.display.set_mode((W,H))
 color= (255,255,255)
 red = (255 , 0 , 0 )
-x=0
-y=0
+blue  = (0,0,255)
+
 w=30
 h=30
-movex = 0
-movey = 0
 
-frog=pygame.image.load(r'pygame\frog.png')#raw string-path
+
+frog=pygame.image.load("frog.png")#raw string-path
 frog = pygame.transform.scale(frog,(50,50))
-frogX = random.randint(0,W-50)
-frogY = random.randint(0,H-50)
 
-while True:
-    gameScreen.fill(color)
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type==pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                movex=-1
-                movey=0
-            elif event.key == pygame.K_RIGHT:
-                movex=1
-                movey=0
-            elif event.key==pygame.K_UP:
-                movey=-1
-                movex=0
-            elif event.key==pygame.K_DOWN:
-                movey=1
-                movex=0
-    # gameScreen.blit(image,(imageX,imageY))
-    frogRect = pygame.Rect([frogX,frogY,50,50])
-    gameScreen.blit(frog,(frogX,frogY))
-    x += movex
-    y += movey
-    
-    if x>W-w:
-        movex=-1
-    elif x<0:
-        movex=1
-    if y>H-h:
-        movey=-1
-    elif y<0:
-        movey=1
-    
-    snake = pygame.draw.rect(gameScreen,red,[x,y,w,h])
-    if snake.colliderect(frogRect):
-        frogX = random.randint(0,W-50)
-        frogY = random.randint(0,H-50)
 
+audio = pygame.mixer.Sound("point.wav")
+
+def Score(counter):
+    font=pygame.font.SysFont(None,30)
+    #anti aliasing ->texture-> True
+    text=font.render(f"Score : {counter}",True,blue)
+    gameScreen.blit(text,(10,10))
+
+def Snake(snakeList):
+    for i in snakeList:
+        pygame.draw.rect(gameScreen,red,[i[0],i[1],w,h])
+
+def main():
+    movex = 0
+    movey = 0
+    frogX = random.randint(0,W-50)
+    frogY = random.randint(0,H-50)
+    x=0
+    y=0
+    counter=0
+    snakeList= []
+    snakeLength=1
+    while True:
+        gameScreen.fill(color)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
         
-    pygame.display.update()
+            if event.type==pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    movex=-1
+                    movey=0
+            
+                elif event.key == pygame.K_RIGHT:
+                    movex=1
+                    movey=0
+            
+                elif event.key==pygame.K_UP:
+                    movey=-1
+                    movex=0
+            
+                elif event.key==pygame.K_DOWN:
+                    movey=1
+                    movex=0
+        # gameScreen.blit(image,(imageX,imageY))
+        snake = pygame.draw.rect(gameScreen,red,[x,y,w,h])
+        snakeList.append([x,y])
+
+        Snake(snakeList)
+        
+        frogRect = pygame.Rect([frogX,frogY,50,50])
+        gameScreen.blit(frog,(frogX,frogY))
+
+        x += movex
+        y += movey
+        
+        if x>W-w:
+            movex=-1
+        elif x<0:
+            movex=1
+        if y>H-h:
+            movey=-1
+        elif y<0:
+            movey=1
+            
+        Score(counter)
+        
+        if snakeLength<len(snakeList):
+            del snakeList[0]
+            
+        if snake.colliderect(frogRect):
+            frogX = random.randint(0,W-50)
+            frogY = random.randint(0,H-50)
+            counter+=1
+            audio.play()
+            snakeLength+=20
+        pygame.display.update()
+        
+        
+main()
